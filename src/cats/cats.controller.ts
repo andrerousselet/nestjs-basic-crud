549cb2aaa0f5e843f13090b8245fdd79afee39c0
Andre Rousselet <andre098@hotmail.com>
2023-02-07 14:21:18 -0300

    feat(cats controller): add POST /cats and GET /cats/:id endpoints

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
